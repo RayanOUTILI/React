66fa22ff51efcf9c9f5712054fe94c82258dfdbe
Outili Rayan <rayan.outili@etu.unice.fr>
2023-05-29 19:46:55 +0200

    components

--- src/App.js
+++ src/App.js
@@ -2,19 +2,47 @@ import "./App.css";
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./pages/Main";
+import Text from "./pages/Text";
+import Logo from "./components/Logo";
 
 function App() {
   return (
     <div className="App">
+      {/* on fait un menu de navigation  */}
+      <nav className="navbar">
+        <ul>
+          <li>
+            <a href="/logo">Logo</a>
+          </li>
+          <li>
+            <a href="/Text">Text</a>
+          </li>
+        </ul>
+      </nav>
+      <h1>Ceci est un test</h1>
+      {/* lorem ipsum */}
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+        voluptatum, quibusdam, quia, quae voluptate voluptatem quod voluptatibus
+      </p>
+
+      {/* Ici je veux injecter la suite du lorem avant le logo  */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/Text" element={<Text />} />
+        </Routes>
+      </BrowserRouter>
+
+      {/* ici on peut utiliser le composant logo (permet de coder un composant 1 seule fois) */}
+      <Logo />
+
+      {/* Permet d'indiquer l'endroit de l'injection de code (ici à la fin) */}
       <BrowserRouter>
         <Routes>
           <Route path="/Main" element={<Main />} />
+          <Route path="/Logo" element={<Logo />} />
         </Routes>
       </BrowserRouter>
-      <h1>Hi, I'm a React App</h1>
-      <p>This is really working!</p>
-      {/* on met un lien vers main */}
-      <a href="/Main">Main</a>
     </div>
   );
 }
---
+++ src/pages/Text.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Text = () => {
+  return (
+    <div className="Text">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+        voluptatum, quibusdam, quia, quae voluptate voluptatem quod voluptatibus
+        quos doloribus quidem voluptas. Quisquam, quibusdam, quia, quae
+        voluptate voluptatem quod voluptatibus quos doloribus quidem voluptas.
+      </p>
+    </div>
+  );
+};
+
+export default Text;
